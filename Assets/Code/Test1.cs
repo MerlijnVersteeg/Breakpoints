cfab6d7b4a2fdbc8a36d5fbaa03f6e8e6bbe97a8
fisser <daniel_pels@hotmail.com>
2015-11-30 13:32:42 +0100

    fix test1

--- Assets/Code/Test1.cs
+++ Assets/Code/Test1.cs
@@ -7,14 +7,30 @@ public class Test1 : MonoBehaviour
 
     void OnGUI()
     {
-        GUI.Label(new Rect(10, 10, 400, 200),"Als de twee cubes elkaar raken moet de OnCollision log komen.\nGebruik alleen maar breakpoints geen code aanpassen.");
+        GUI.Label(new Rect(10, 10, 400, 200), "Als de twee cubes elkaar raken moet de OnCollision log komen.\nGebruik alleen maar breakpoints geen code aanpassen.");
     }
 
     void OnCollisionEnter(Collision coll)
     {
-        if (coll.gameObject.tag == "Cube")
+        if (checkCollision(coll.gameObject.tag))
         {
-            Debug.Log("Hit");
+            Debug.Log("Cube");
+        }
+        else
+        {
+            Debug.Log("errr");
+        }
+    }
+
+    bool checkCollision(string tag)
+    {
+        if (tag == "Cube")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 }
